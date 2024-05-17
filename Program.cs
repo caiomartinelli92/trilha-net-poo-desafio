bf3e515e159fdc1ef8930298369ed86a2f58538f
Caio Martinelli <caio.martinelli@gmail.com>
2024-05-17 19:24:41 -0300

    Versao final do projeto de Sistemas de Smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("123456", "Tijolao", "111222333", 8);
+Iphone i1 = new Iphone("7891011", "15 MAX PRO", "777888999", 16);
+
+Console.WriteLine("Numero do Nokia é: " + n1.Numero);
+Console.WriteLine("Numero do Nokia é: " + i1.Numero);
+
+n1.InstalarAplicativo("Instagram");
+i1.InstalarAplicativo("Uber");
